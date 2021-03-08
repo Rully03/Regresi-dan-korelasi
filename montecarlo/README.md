954235d7c96dbdce2a3b563144e0dcc159f8d4d8
Rully03 <67133106+Rully03@users.noreply.github.com>
2021-03-08 23:13:43 +0700

    Add files via upload

---
+++ montecarlo/README.md
@@ -0,0 +1,4 @@
+# PEMSIM COLLECTIONS
+Kumpulan tugas-tugas pemrograman simulasi
+
+Minta?? Ijin dulu bosss
---
+++ montecarlo/montecarlo.py
@@ -0,0 +1,83 @@
+#INI ADALAH PROGRAM SIMULASI MONTECARLO#
+#Nama : Rully Angeana B
+#NRP : 152018077
+#-----------------------------------------#
+import random
+def inputdata():
+    while True:
+        entry1 = input('Permintaan = ')
+        entry2 = input('Minggu : ')
+        if (entry1 == '') and (entry2 == ''):
+                break
+        try:
+            datainput.append((int(entry1),int(entry2)))
+        except:
+            print("Masukan Angka!")
+
+def prob():
+    ftot = 0
+    out = []
+    for x in range(len(datainput)):
+        ftot += datainput[x][1]
+    for x in range(len(datainput)):
+        n = datainput[x][1] / ftot
+        out.append(n)
+    return out
+
+def probkum(inprob):
+    out = []
+    out.append(inprob[0])
+    panjang = len(inprob) - 1
+    for x in range(panjang):
+        n = out[x] + inprob[x+1]
+        out.append(round(n,1))
+    return out
+
+def interval(inprobkum):
+    atas = []
+    bawah = []
+    bawah.append(0)
+    for x in range((len(inprobkum))-1):
+        n = inprobkum[x] + 0.001
+        bawah.append(n)
+    for x in range((len(inprobkum))):
+        n = inprobkum[x]
+        atas.append(n)
+    return bawah, atas
+
+def predict(banyaknya, bawah, atas, harga):
+    total1 = 0
+    total2 = 0
+    data = []
+    out = []
+    for x in range(banyaknya):
+        data.append(random.random())
+    for x in range(banyaknya):
+        if (data[x] >= bawah[0]) and (data[x] <= atas[0]):
+            n=0
+        elif (data[x] >= bawah[1]) and (data[x] <= atas[1]):
+            n=1
+        elif (data[x] >= bawah[2]) and (data[x] <= atas[2]):
+            n=2
+        elif (data[x] >= bawah[3]) and (data[x] <= atas[3]):
+            n=3
+        elif (data[x] >= bawah[4]) and (data[x] <= atas[4]):
+            n=4
+        out.append(n)
+    data = []
+    for x in  range(len(out)):
+        total1 += out[x]
+        n = out[x] * harga
+        total2 += n
+    print('Prediksi Permintaan Sebanyak = ', total1, 'pcs')
+    print('Prediksi Pengeluaran = Rp.',total2)
+
+datainput = []
+inputdata()
+alpha = prob()
+beta = probkum(alpha)
+bo, ab = interval(beta)[:2]
+npredict = int(input('Masukkan banyak prediksi: '))
+price = int(input('Tentukan modal barang: '))
+print('== Prediksi Dengan MonteCarlo ==')
+predict(npredict, bo, ab, price)
